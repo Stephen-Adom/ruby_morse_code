a1e8e37874b829590fdee9406092b2f54a5f31d3
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-05-10 16:17:05 +0300

    add decode method for word and full message

--- morse_code.rb
+++ morse_code.rb
@@ -36,5 +36,19 @@ class MorseDecoder
     morse_arr = morse_word.split(' ')
     create_word(morse_arr)
   end
-
+def decode_full_message(message)
+    decoded_message = ''
+    message_words = message.split("   ")
+    message_words.each do |word|
+      char_array = word.split(" ")
+      decoded_message << " " << create_word(char_array)
+    end
+    decoded_message
+  end
+  private
+  def create_word(morse_array)
+    word = ''
+    morse_array.each {|morse| word << decode_char(morse)}
+    word
+  end
 end
